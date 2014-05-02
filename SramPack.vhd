6a78c7baf9740a18b1f737dd58aba58d2bb74754
Erik Zachrisson <erik@zachrisson.info>
2014-05-02 08:05:35 +0200

    Reindent. No functional change

---
+++ SramPack.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+use work.Types.all;
+
+package SramPack is
+  constant SramDataW : positive := 16;
+  constant SramAddrW : positive := 18;
+
+end package;
+
+package body SramPack is
+
+end package body;
+ 
